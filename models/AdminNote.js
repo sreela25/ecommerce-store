2ee5a31e5d66bae74be08eac2e5e1840ab557372
sreela25 <sreela2507@gmail.com>
2026-09-21 23:34:28 +0530

    Fix Admin Note

--- models/AdminNote.js
+++ models/AdminNote.js
@@ -8,3 +8,22 @@ const adminNoteSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("AdminNote", adminNoteSchema);
+
+router.get("/", async (req, res) => {
+    try {
+        let note = await AdminNote.findOne();
+
+        if (!note) {
+            note = await AdminNote.create({
+                message: ""
+            });
+        }
+
+        res.json(note);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
